13a4471ba1f278aae91f1b52c32f223e195fd379
doubleLLX <72087835+doubleLLX@users.noreply.github.com>
2020-11-26 00:06:52 -0500

    Update myScript.js

---
+++ myScript.js
@@ -0,0 +1,6 @@
+function getInfo(){
+    var name = parseInt(document.getElementById("Name").value);
+    var suggest = parseInt(document.getElementById("Suggestion").value);
+    
+    alert("Thank you for your suggestion" + name);
+}
