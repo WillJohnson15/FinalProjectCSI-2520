720956c6995adb125c2f8258ebace79ce9e6c8a4
Kramenator <72082939+Kramenator@users.noreply.github.com>
2020-11-26 00:39:15 -0500

    added newsletter function beta

--- myScript.js
+++ myScript.js
@@ -1,6 +1,12 @@
-function getInfo(){
-    var name = parseInt(document.getElementById("Name").value);
-    var suggest = parseInt(document.getElementById("Suggestion").value);
+function getInfo() {
+    var name = document.getElementById("Name").value;
+    var suggest = document.getElementById("Suggestion").value;
     
-    alert("Thank you for your suggestion" + name);
+    alert("Thank you for your suggestion " + name + ".");
+}
+
+function newsLetter() {
+    var email = document.getElementById("Email").value;
+    
+    alert("Thank you for signing up for our newsletter! Please check " + email + " for confirmation");
 }
